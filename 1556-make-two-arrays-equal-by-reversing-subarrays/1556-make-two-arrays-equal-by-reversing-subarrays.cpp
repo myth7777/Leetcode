3172a2920fe45ba730bcbd377b2b9eb88af5749c
Myth <101268873+myth7777@users.noreply.github.com>
2024-08-03 13:24:27 +0530

    Time: 14 ms (24.92%), Space: 19.5 MB (23.36%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        if(target.size()==1 && target[0]==arr[0])
+        // if(target.size()==1 && target[0]==arr[0])
+        //     return true;
+        
+        // sort(target.begin(), target.end());
+        // sort(arr.begin(), arr.end());
+
+        // for(int i=0; i<target.size(); i++)
+        // {
+        //     if(target[i]!=arr[i])
+        //         return false;
+        // }
+        // return true;
+        int n = target.size();
+        map<int,int>mp;
+        for(auto it:target)
+            mp[it]++;
+
+        for(int num:arr)
+        {
+            if(mp.find(num)==mp.end())
+                return false;
+            
+            mp[num]--;
+            if(mp[num]==0)
+                mp.erase(num);
+        }
+
+        if(mp.size()==0)
             return true;
         
-        sort(target.begin(), target.end());
-        sort(arr.begin(), arr.end());
-
-        for(int i=0; i<target.size(); i++)
-        {
-            if(target[i]!=arr[i])
-                return false;
-        }
-        return true;
+        return false;
     }
 };
