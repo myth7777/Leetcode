d0dfea172122da19a39ceae8c01e5d173244a752
Myth <101268873+myth7777@users.noreply.github.com>
2024-09-25 23:41:29 +0530

    Time: 15 ms (6.14%), Space: 10 MB (16.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -14,10 +14,22 @@ public:
         convertTolower(s);
         convertTolower(t);
         
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
 
-        return s==t;
+        // return s==t;
+
+        map<char, int>char_count;
+
+        for(char ch : s)
+            char_count[ch]++;
         
+        for (char ch : t)
+        {
+            char_count[ch]--;
+            if(char_count[ch]<0)
+                return false;
+        }
+        return true;
     }
 };
