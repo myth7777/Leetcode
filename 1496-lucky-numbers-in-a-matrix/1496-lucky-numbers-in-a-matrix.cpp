7877ebbc5c84ebe5e32c09f9d9404900a726e445
Myth <101268873+myth7777@users.noreply.github.com>
2024-09-27 17:39:58 +0530

    Time: 20 ms (21.56%), Space: 14.3 MB (86.87%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
@@ -6,39 +6,68 @@ public:
         m = matrix.size();
         n = matrix[0].size();
         
-        vector<int>min_row;
-        vector<int>max_col;
+        for(int i=0; i<m; i++)
+        {
+            int min_in_row = INT_MAX;
+            int min_col_idx = -1;
 
-        for(int i=0; i<m; i++)
-        {
-            int mini=INT_MAX;
             for(int j=0; j<n; j++)
             {
-                if(mini>matrix[i][j])
-                    mini = matrix[i][j];
+                if(min_in_row > matrix[i][j])
+                {
+                    min_in_row = matrix[i][j];
+                    min_col_idx = j;
+                }
             }
-            min_row.push_back(mini);
-        }
-        
-        for(int j=0; j<n; j++)
-        {
-            int maxi=INT_MIN;
-            for(int i=0; i<m; i++)
+
+            bool is_max_in_col = true;
+            for(int k=0; k<m; k++)
             {
-                if(maxi<matrix[i][j])
-                    maxi = matrix[i][j];
+                if(matrix[k][min_col_idx] > min_in_row)
+                {
+                    is_max_in_col = false;
+                    break;
+                }
             }
-            max_col.push_back(maxi);
+            if(is_max_in_col)
+                ans.push_back(min_in_row);
         }
-        for(int i=0; i<m; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                if((matrix[i][j] == min_row[i]) && (matrix[i][j] == max_col[j]))
-                    ans.push_back(matrix[i][j]);
-            }
-        }
-        
+
         return ans;
+
+
+        // vector<int>min_row;
+        // vector<int>max_col;
+
+        // for(int i=0; i<m; i++)
+        // {
+        //     int mini=INT_MAX;
+        //     for(int j=0; j<n; j++)
+        //     {
+        //         if(mini>matrix[i][j])
+        //             mini = matrix[i][j];
+        //     }
+        //     min_row.push_back(mini);
+        // }
+        
+        // for(int j=0; j<n; j++)
+        // {
+        //     int maxi=INT_MIN;
+        //     for(int i=0; i<m; i++)
+        //     {
+        //         if(maxi<matrix[i][j])
+        //             maxi = matrix[i][j];
+        //     }
+        //     max_col.push_back(maxi);
+        // }
+        // for(int i=0; i<m; i++)
+        // {
+        //     for(int j=0; j<n; j++)
+        //     {
+        //         if((matrix[i][j] == min_row[i]) && (matrix[i][j] == max_col[j]))
+        //             ans.push_back(matrix[i][j]);
+        //     }
+        // }
+        // return ans;
     }
 };
