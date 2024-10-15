4024c3f0e37da58c8ffb1f0fdc5d9491a355bf16
Myth <101268873+myth7777@users.noreply.github.com>
2024-10-15 22:21:30 +0530

    Time: 0 ms (100%), Space: 10.1 MB (74.15%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
@@ -2,12 +2,23 @@ class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
         int n = gain.size();
-        vector<int> prefixSum(n+1, 0);
-        
-        for(int i=1; i<=n; i++)
+        int maxAlt = 0;
+        int currAlt = 0;
+        for(int i=0; i<n; i++)
         {
-             prefixSum[i] = prefixSum[i-1] + gain[i-1];
+            currAlt += gain[i];
+            maxAlt = max(currAlt, maxAlt);
         }
-        return *max_element(prefixSum.begin(), prefixSum.end());
+
+        return maxAlt;
+
+
+        // vector<int> prefixSum(n+1, 0);
+        
+        // for(int i=1; i<=n; i++)
+        // {
+        //      prefixSum[i] = prefixSum[i-1] + gain[i-1];
+        // }
+        // return *max_element(prefixSum.begin(), prefixSum.end());
     }
 };
