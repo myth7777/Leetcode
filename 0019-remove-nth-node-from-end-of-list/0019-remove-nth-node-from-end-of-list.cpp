ef9bba164f9f45007d1338fe13a84dc1b9823fcf
Myth <101268873+myth7777@users.noreply.github.com>
2024-10-19 16:21:18 +0530

    Time: 0 ms (100%), Space: 14.8 MB (34.11%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,38 +11,61 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head == NULL)
-            return NULL;
-        
-        int cnt=0;
-        ListNode *temp = head;
+        // FAST and SLOW pointer
+        ListNode *fast = head;
+        for(int i=0; i<n; i++)
+            fast = fast->next;
 
-        while(temp != NULL)
+        if(fast == NULL)
+            return head->next;
+
+        ListNode *slow = head;
+
+        while(fast->next != NULL)
         {
-            cnt++;
-            temp = temp->next;
+            fast = fast->next;
+            slow = slow->next;
         }
 
-        if(n==cnt)
-        {
-            ListNode *newHead = head->next;
-            delete head;
-            return newHead;
-        }
-        int res = cnt - n;
-        temp = head;
-        while(temp!=NULL)
-        {
-            res--;
-            if(res == 0)
-                break;
-            temp = temp->next;
-        }
-
-        ListNode *delNode = temp->next;
-        temp->next = temp->next->next;
+        ListNode *delNode = slow->next;
+        slow->next = slow->next->next;
         delete delNode;
 
-        return head;
+        return head;        
+
+        // brute Forece
+        // if(head == NULL)
+        //     return NULL;
+        
+        // int cnt=0;
+        // ListNode *temp = head;
+
+        // while(temp != NULL)
+        // {
+        //     cnt++;
+        //     temp = temp->next;
+        // }
+
+        // if(n==cnt)
+        // {
+        //     ListNode *newHead = head->next;
+        //     delete head;
+        //     return newHead;
+        // }
+        // int res = cnt - n;
+        // temp = head;
+        // while(temp!=NULL)
+        // {
+        //     res--;
+        //     if(res == 0)
+        //         break;
+        //     temp = temp->next;
+        // }
+
+        // ListNode *delNode = temp->next;
+        // temp->next = temp->next->next;
+        // delete delNode;
+
+        // return head;
     }
 };
