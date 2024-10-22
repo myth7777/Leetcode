28c3ba06930a70982e83e2cff0c0ec178bc31b34
Myth <101268873+myth7777@users.noreply.github.com>
2024-10-22 23:51:01 +0530

    Time: 1434 ms (27.31%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -5,6 +5,11 @@
 -- left join Product as P
 -- on S.product_id = P.product_id;
 
-SELECT p.product_name, s.year, s.price
-FROM Product AS p, Sales AS s
-WHERE p.product_id = s.product_id
+-- SELECT p.product_name, s.year, s.price
+-- FROM Product AS p, Sales AS s
+-- WHERE p.product_id = s.product_id
+
+SELECT P.product_name, S.year, S.price 
+FROM Sales S 
+JOIN Product P 
+ON S.product_id = P.product_id;
