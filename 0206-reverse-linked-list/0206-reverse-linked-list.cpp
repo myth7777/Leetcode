c36993ab57ea77c17cdf7a433439cb15835eca3e
Myth <101268873+myth7777@users.noreply.github.com>
2024-10-26 22:18:07 +0530

    Time: 0 ms (100%), Space: 13.3 MB (8.36%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,16 +11,28 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-            ListNode *temp = head;
-            ListNode *prev = NULL;
+        // ------------- RECURSIVE METHOD --------------
+        if(head == NULL || head->next == NULL)
+            return head;
 
-            while(temp!=NULL)
-            {
-                ListNode *front = temp->next;
-                temp->next = prev;
-                prev = temp;
-                temp = front;
-            }
-        return prev;
+        ListNode *newHead = reverseList(head->next);
+        ListNode *front = head->next;
+        front->next = head;
+        head->next = NULL;
+
+        return newHead;
+
+        // ------------- ITERATIVE MENTHOD -------------
+        // ListNode *temp = head;
+        // ListNode *prev = NULL;
+
+        // while(temp!=NULL)
+        // {
+        //     ListNode *front = temp->next;
+        //     temp->next = prev;
+        //     prev = temp;
+        //     temp = front;
+        // }
+        // return prev;
     }
 };
