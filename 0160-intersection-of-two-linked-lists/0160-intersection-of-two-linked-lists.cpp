9b4d8ab6c9e6224830d2c9af6116897ce65bdb41
Myth <101268873+myth7777@users.noreply.github.com>
2024-10-27 01:03:50 +0530

    Time: 40 ms (41.79%), Space: 17.3 MB (65.2%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,24 +9,63 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        map<ListNode*, int>mpp;
-        ListNode *temp1 = headA;
-        ListNode *temp2 = headB;
-
-        while(temp1 != NULL)
+        // ----------DIFFERENCE IN LENGTH-----------
+        ListNode *t1 = headA;
+        ListNode *t2 = headB;
+        int n1=0, n2=0;
+        while(t1 != NULL)
         {
-            mpp[temp1] = 1;
-            temp1 = temp1->next;
+            n1++;
+            t1 = t1->next;
+        }
+        while(t2 != NULL)
+        {
+            n2++;
+            t2 = t2->next;
+        }
+        t1 = headA;
+        t2 = headB;
+        int d;
+        if(n2>n1)
+        {
+            d = n2-n1;
+            while(d--)
+                t2 = t2->next;
+        }
+        else
+        {
+            d = n1-n2;
+            while(d--)
+                t1=t1->next;
         }
 
-        while(temp2 != NULL)
+        while(t1 != NULL)
         {
-            if(mpp.find(temp2)!=mpp.end())
-            {
-                return temp2;
-            }
-            temp2 = temp2->next;
+            if(t1 == t2)
+                return t1;
+            t1 = t1->next;
+            t2 = t2->next;
         }
         return NULL;
+        // ----------USING MAP---------
+        // map<ListNode*, int>mpp;
+        // ListNode *temp1 = headA;
+        // ListNode *temp2 = headB;
+
+        // while(temp1 != NULL)
+        // {
+        //     mpp[temp1]++;
+        //     temp1 = temp1->next;
+        // }
+
+        // while(temp2 != NULL)
+        // {
+        //     if(mpp.find(temp2)!=mpp.end())
+        //     {
+        //         return temp2;
+        //     }
+        //     temp2 = temp2->next;
+        // }
+        // return NULL;
     }
 };
