c18cc8c735d3caa725fbeeb702eae7780da36c48
Myth <101268873+myth7777@users.noreply.github.com>
2024-10-27 14:13:15 +0530

    Time: 38 ms (54.21%), Space: 17.3 MB (85.63%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,44 +9,67 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        // ----------DIFFERENCE IN LENGTH-----------
+        // ----------OPTIMIZED CODE-------------
         ListNode *t1 = headA;
         ListNode *t2 = headB;
-        int n1=0, n2=0;
-        while(t1 != NULL)
-        {
-            n1++;
-            t1 = t1->next;
-        }
-        while(t2 != NULL)
-        {
-            n2++;
-            t2 = t2->next;
-        }
-        t1 = headA;
-        t2 = headB;
-        int d;
-        if(n2>n1)
-        {
-            d = n2-n1;
-            while(d--)
-                t2 = t2->next;
-        }
-        else
-        {
-            d = n1-n2;
-            while(d--)
-                t1=t1->next;
-        }
 
-        while(t1 != NULL)
+        while(t1 != t2)
         {
+            t1 = t1->next;
+            t2 = t2->next;
+
             if(t1 == t2)
                 return t1;
-            t1 = t1->next;
-            t2 = t2->next;
+            
+            if(t1 == NULL)
+                t1 = headB;
+            
+            if(t2 == NULL)
+                t2 = headA;
         }
-        return NULL;
+        return t1;
+        
+        
+        // // ----------DIFFERENCE IN LENGTH-----------
+        // ListNode *t1 = headA;
+        // ListNode *t2 = headB;
+        // int n1=0, n2=0;
+        // while(t1 != NULL)
+        // {
+        //     n1++;
+        //     t1 = t1->next;
+        // }
+        // while(t2 != NULL)
+        // {
+        //     n2++;
+        //     t2 = t2->next;
+        // }
+        // t1 = headA;
+        // t2 = headB;
+        // int d;
+        // if(n2>n1)
+        // {
+        //     d = n2-n1;
+        //     while(d--)
+        //         t2 = t2->next;
+        // }
+        // else
+        // {
+        //     d = n1-n2;
+        //     while(d--)
+        //         t1=t1->next;
+        // }
+
+        // while(t1 != NULL)
+        // {
+        //     if(t1 == t2)
+        //         return t1;
+        //     t1 = t1->next;
+        //     t2 = t2->next;
+        // }
+        // return NULL;
+
+
         // ----------USING MAP---------
         // map<ListNode*, int>mpp;
         // ListNode *temp1 = headA;
