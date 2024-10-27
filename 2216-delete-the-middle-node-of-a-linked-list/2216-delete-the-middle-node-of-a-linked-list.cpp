f4ed285a9b740abad4b74e81a1b3bd368cce3d32
Myth <101268873+myth7777@users.noreply.github.com>
2024-10-27 14:39:24 +0530

    Time: 0 ms (100%), Space: 311.9 MB (9.27%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.cpp
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.cpp
@@ -11,43 +11,63 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        int len=0;
-        ListNode *temp1 = head;
-
-        while(temp1 != nullptr)
+        // ----------FAST AND SLOW POINTER------------
+        ListNode *slow = head;
+        ListNode *fast = head;
+        ListNode *prev = NULL;
+        if (head == NULL || head->next == NULL) 
         {
-            temp1 = temp1->next;
-            len++;
+            return NULL;
         }
-        int mid = (len/2);
-
-        if(head == nullptr || head->next == nullptr)
-            return nullptr;
-        
-        if(mid==0)
+        while(fast != NULL && fast->next != NULL)
         {
-            ListNode* temp = head;
-            head = head->next;
-            delete temp;
-            return head;
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
         }
+        prev->next = slow->next;
 
-        ListNode *temp = head;
-        ListNode *prev = nullptr;
-        int cnt=-1;
-
-        while(temp != nullptr)
-        {
-            cnt++;
-            if(cnt==mid)
-            {
-                prev->next = prev->next->next;
-                delete temp;
-                break;
-            }
-            prev = temp;
-            temp = temp->next;
-        }
         return head;
+
+
+        // // -----------BRUTE FORCE-----------
+        // int len=0;
+        // ListNode *temp1 = head;
+
+        // while(temp1 != nullptr)
+        // {
+        //     temp1 = temp1->next;
+        //     len++;
+        // }
+        // int mid = (len/2);
+
+        // if(head == nullptr || head->next == nullptr)
+        //     return nullptr;
+        
+        // if(mid==0) 
+        // {
+        //     ListNode* temp = head;
+        //     head = head->next;
+        //     delete temp;
+        //     return head;
+        // }
+
+        // ListNode *temp = head;
+        // ListNode *prev = nullptr;
+        // int cnt=-1;
+
+        // while(temp != nullptr)
+        // {
+        //     cnt++;
+        //     if(cnt==mid)
+        //     {
+        //         prev->next = prev->next->next;
+        //         delete temp;
+        //         break;
+        //     }
+        //     prev = temp;
+        //     temp = temp->next;
+        // }
+        // return head;
     }
 };
