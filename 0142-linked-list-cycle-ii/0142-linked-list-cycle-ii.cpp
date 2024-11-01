53ac0864a870dda7eb03e0f81a6eb36ee598b413
Myth <101268873+myth7777@users.noreply.github.com>
2024-11-01 11:27:24 +0530

    Time: 10 ms (34.6%), Space: 10.4 MB (33.16%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,23 +9,47 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
+        // //--------------USING FAST & SLOW POINTER-----------------
         if(head == NULL || head->next == NULL)
             return NULL;
         
-        map<ListNode*, int>mpp;
-
-        ListNode *temp = head;
-
-        while(temp!=NULL)
-        {
-            if(mpp.find(temp)!=mpp.end())
-            {
-                return temp;
-            }
-            mpp[temp]++;
-            temp = temp->next;
-        }
+        ListNode *fast = head;
+        ListNode *slow = head;
         
+        while(fast != NULL && fast->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if(fast == slow)
+            {
+                slow = head;
+                while(fast != slow)
+                {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
         return NULL;
+        // //--------------USING HASHING----------------
+        // if(head == NULL || head->next == NULL)
+        //     return NULL;
+        
+        // map<ListNode*, int>mpp;
+
+        // ListNode *temp = head;
+
+        // while(temp!=NULL)
+        // {
+        //     if(mpp.find(temp)!=mpp.end())
+        //     {
+        //         return temp;
+        //     }
+        //     mpp[temp]++;
+        //     temp = temp->next;
+        // }
+        // return NULL;
     }
 };
