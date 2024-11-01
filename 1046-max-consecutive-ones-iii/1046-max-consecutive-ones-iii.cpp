6807872185c33bb3a2bdbb2d112db683cc3d0182
Myth <101268873+myth7777@users.noreply.github.com>
2024-11-01 16:48:52 +0530

    Time: 0 ms (100%), Space: 58 MB (86.17%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        // ---------SLIDING WINDOW----------
+        // ----------OPTIMAL WAY----------
         int n = nums.size();
         int maxLen = 0;
         int l=0, r=0, zeros=0;
@@ -11,16 +11,39 @@ public:
             if(nums[r]==0)
                 zeros++;
             
-            while(zeros>k)
+            if(zeros>k)
             {
                 if(nums[l]==0)
                     zeros--;
                 l++;
             }
-            maxLen = max(maxLen, r-l+1);
+            if(zeros<=k)
+                maxLen = max(maxLen, r-l+1);
+
             r++;
         }
         return maxLen;
+        
+        // // ---------SLIDING WINDOW----------
+        // int n = nums.size();
+        // int maxLen = 0;
+        // int l=0, r=0, zeros=0;
+
+        // while(r<n)
+        // {
+        //     if(nums[r]==0)
+        //         zeros++;
+            
+        //     while(zeros>k)
+        //     {
+        //         if(nums[l]==0)
+        //             zeros--;
+        //         l++;
+        //     }
+        //     maxLen = max(maxLen, r-l+1);
+        //     r++;
+        // }
+        // return maxLen;
         
         // //--------BRUTE FORCE--------
         // int n=nums.size();
