ee1fea3a25febb44adb174782c127c01ffc7e6c2
Myth <101268873+myth7777@users.noreply.github.com>
2024-11-02 10:41:24 +0530

    Time: 468 ms (53.63%), Space: 0B (100%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,2 +1,6 @@
 # Write your MySQL query statement below
-select distinct author_id as id from Views where author_id = viewer_id order by id;
+SELECT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+GROUP BY author_id
+ORDER BY author_id;
