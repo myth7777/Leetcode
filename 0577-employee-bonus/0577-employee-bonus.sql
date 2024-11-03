cb58f7e776fa228b776bfa4893038162293bc1fa
Myth <101268873+myth7777@users.noreply.github.com>
2024-11-03 19:18:18 +0530

    Time: 1101 ms (49.73%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-select e.name, b.bonus
-from Employee e
-left join Bonus b
-on e.empId = b.empId
-where b.bonus < 1000 or b.bonus is NULL
+SELECT e.name, b.bonus
+FROM Employee e
+LEFT JOIN Bonus b
+ON e.empId = b.empId
+WHERE b.bonus < 1000 OR b.bonus is NULL;
