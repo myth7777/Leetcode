a99732a572897bce6a6d4ffef592ab4d84ef4b18
Myth <101268873+myth7777@users.noreply.github.com>
2024-11-08 12:03:57 +0530

    Time: 15 ms (8.11%), Space: 13.8 MB (8.92%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -13,43 +13,43 @@ public:
         // if(head == NULL || head->next == NULL)
         //     return NULL;
         
-        ListNode *fast = head;
-        ListNode *slow = head;
+        // ListNode *fast = head;
+        // ListNode *slow = head;
         
-        while(fast != NULL && fast->next != NULL)
-        {
-            fast = fast->next->next;
-            slow = slow->next;
+        // while(fast != NULL && fast->next != NULL)
+        // {
+        //     fast = fast->next->next;
+        //     slow = slow->next;
 
-            if(fast == slow)
-            {
-                slow = head;
-                while(fast != slow)
-                {
-                    slow = slow->next;
-                    fast = fast->next;
-                }
-                return slow;
-            }
-        }
-        return NULL;
+        //     if(fast == slow)
+        //     {
+        //         slow = head;
+        //         while(fast != slow)
+        //         {
+        //             slow = slow->next;
+        //             fast = fast->next;
+        //         }
+        //         return slow;
+        //     }
+        // }
+        // return NULL;
         // //--------------USING HASHING----------------
         // if(head == NULL || head->next == NULL)
         //     return NULL;
         
-        // map<ListNode*, int>mpp;
+        map<ListNode*, int>mpp;
 
-        // ListNode *temp = head;
+        ListNode *temp = head;
 
-        // while(temp!=NULL)
-        // {
-        //     if(mpp.find(temp)!=mpp.end())
-        //     {
-        //         return temp;
-        //     }
-        //     mpp[temp]++;
-        //     temp = temp->next;
-        // }
-        // return NULL;
+        while(temp!=NULL)
+        {
+            if(mpp.find(temp)!=mpp.end())
+            {
+                return temp;
+            }
+            mpp[temp]++;
+            temp = temp->next;
+        }
+        return NULL;
     }
 };
