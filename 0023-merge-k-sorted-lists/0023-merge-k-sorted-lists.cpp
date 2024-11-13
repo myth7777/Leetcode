609ce8dfdfc932938e9c5a4951de543a061417af
Myth <101268873+myth7777@users.noreply.github.com>
2024-11-13 20:24:19 +0530

    Time: 8 ms (30.87%), Space: 18.6 MB (35.81%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -26,42 +26,68 @@ public:
     //     return head;
     // }
 
-    ListNode* merge(ListNode* head1, ListNode* head2)
-    {
-        ListNode* temp1 = head1;
-        ListNode* temp2 = head2;
+    // ListNode* merge(ListNode* head1, ListNode* head2)
+    // {
+    //     ListNode* temp1 = head1;
+    //     ListNode* temp2 = head2;
+    //     ListNode* dummy = new ListNode(-1);
+    //     ListNode* temp = dummy;
+
+    //     while(temp1 != NULL && temp2 != NULL)
+    //     {
+    //         if(temp1->val < temp2->val)
+    //         {
+    //             temp->next = temp1;
+    //             temp = temp->next;
+    //             temp1 = temp1->next;        
+    //         }
+    //         else
+    //         {
+    //             temp->next = temp2;
+    //             temp = temp->next;
+    //             temp2 = temp2->next;
+    //         }
+    //     }
+    //     if(temp1) temp->next = temp1;
+    //     if(temp2) temp->next = temp2;
+
+    //     return dummy->next;
+    // }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        // ---------USING PRIORITY QUEUE--------------
+        if (lists.empty()) return nullptr;
+        priority_queue<pair<int, ListNode*>,vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> minHeap;
+
+        for(int i=0; i<lists.size(); i++)
+        {
+            if(lists[i])
+                minHeap.push({lists[i]->val, lists[i]});
+        }
+        
         ListNode* dummy = new ListNode(-1);
         ListNode* temp = dummy;
 
-        while(temp1 != NULL && temp2 != NULL)
+        while(!minHeap.empty())
         {
-            if(temp1->val < temp2->val)
-            {
-                temp->next = temp1;
-                temp = temp->next;
-                temp1 = temp1->next;        
-            }
-            else
-            {
-                temp->next = temp2;
-                temp = temp->next;
-                temp2 = temp2->next;
-            }
-        }
-        if(temp1) temp->next = temp1;
-        if(temp2) temp->next = temp2;
+            pair<int, ListNode*>p = minHeap.top();
+            temp->next = p.second;
+            minHeap.pop();
 
-        return dummy->next;
-    }
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        // ---------OPTIMAL APPROACH------------
-        if (lists.empty()) return nullptr;
-        ListNode *head = lists[0];
-        for(int i=1; i<lists.size(); i++)
-        {
-            head = merge(head, lists[i]);
+            if(p.second->next)
+                minHeap.push({p.second->next->val, p.second->next});
+
+            temp = temp->next;
         }
-        return head;
+        return dummy->next;
+        // // ---------OPTIMAL APPROACH------------
+        // if (lists.empty()) return nullptr;
+        // ListNode *head = lists[0];
+        // for(int i=1; i<lists.size(); i++)
+        // {
+        //     head = merge(head, lists[i]);
+        // }
+        // return head;
+
 
         // //--------BRUTE FORCE-----------
         // vector<int>arr;
